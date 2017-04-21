a51f6f8d83b1f1ade0a0858bf0dab903fa1f503e
djtgithub <13552030296@163.com>
2017-04-21 11:50:48 +0800

    websocket

---
+++ socket/WsServer.js
@@ -0,0 +1,29 @@
+var WebSocketServer=require('ws').Server,
+    wss=new WebSocketServer({'port':9000})
+var clientMap=new Object()
+var i=0
+var arr=['小明','小花','小刚']
+wss.on('connection',function(ws){
+	console.log(ws+'上线')
+	// for(var i=0;i<arr.length;i++){
+	// 	ws.name=arr[arr.length--]
+	// 	clientMap[ws.name]=ws
+	// }
+	ws.name=++i
+	clientMap[ws.name]=ws
+
+	ws.on('message',function(message){
+		broadcast(message,ws)
+	})
+	ws.on('close',function(){
+		global.gc() //调用内存回收
+		console.log('离开');
+	})
+})
+
+
+function broadcast(msg,ws){
+	for (var key in clientMap){
+		clientMap[key].send(ws.name+'说：'+msg)
+	}
+}
